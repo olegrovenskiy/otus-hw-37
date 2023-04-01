f35288748393d7b4cdfc63c75cf5f8b68ba3d784
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-04-01 18:09:26 +0300

    task-3-start

--- otus-hw-37/Program.cs
+++ otus-hw-37/Program.cs
@@ -1,44 +1,5 @@
-﻿using otus_hw_37;
-using System.Threading.Tasks.Dataflow;
+﻿Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
-
-MyLibrary MySummerLibrary = new MyLibrary();
-
-
-
-while (true)
-{
-    Console.WriteLine("Ваши действия: 1 - добавить книгу; 2 - вывести список непрочитанного; 3 - выйти");
-
-    var act = Console.ReadLine();
-
-
-    if (act == "1")
-    {
-        
-        Console.WriteLine("Введите название книги");
-        string Name = Console.ReadLine();
-        MySummerLibrary.AddBook(Name);
-
-    }
-    else if (act == "2")
-    {
-        MySummerLibrary.GetBooks();        
-    }
-
-
-    else if (act == "3")
-
-    {
-        break;
-    }
-    else
-        Console.WriteLine("Wrong input");
-
-}
-
-Console.WriteLine("exit");
 
 
 

2758e327117e4afffcc5a693ee04d9ba03c54de2
o.rovenskiy@dcoa.ru <o.rovenskiy@dcoa.ru>
2023-04-01 12:36:35 +0300

    program 1 - shop

---
+++ otus-hw-37/Customer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otus_hw_37
+{
+    public class Customer
+    {
+
+        public void OnItemChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+
+            switch (e.Action)
+
+            {
+
+
+                case NotifyCollectionChangedAction.Add:
+                    if (e.NewItems?[0] is Item newItem)
+                        Console.WriteLine($"Добавлен новый товар: ID = {newItem.Id}, Название {newItem.Name}");
+                    break;
+               
+                case NotifyCollectionChangedAction.Remove:
+                    if (e.OldItems?[0] is Item oldItem)
+                        Console.WriteLine($"Удалён товар:  {oldItem.Name}");
+                    break ;
+
+
+             }
+        
+        }
+
+
+    
+
+
+
+    }
+
+
+
+
+
+}
--- otus-hw-37/Program.cs
+++ otus-hw-37/Program.cs
@@ -1,28 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using otus_hw_37;
+
 Console.WriteLine("Hello, World!");
+int IdItem = 0;
+
+Customer oleg = new Customer();
+Shop shop = new Shop();
+
+shop.Goods.CollectionChanged += oleg.OnItemChanged;
 
 
 
+while (true)
+{
+    Console.WriteLine("Ваши действия: А - добавить товар, D - удалить товар, Х - выйти из программы");
+
+    var act = Console.ReadLine();
 
 
+    if (act == "A")
+    {
+        Item newItem = new Item(IdItem, DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+        shop.Add(newItem);
+        IdItem++;
+
+    }
+    else if (act == "D")
+    {
+        Console.WriteLine("Товар с каким ID удалить ?");
+        string? input = Console.ReadLine();
+        bool result = int.TryParse(input, out var number);
+        if (result == true)
+            shop.Remove(number);
+        else
+            Console.WriteLine("Надо ввести ID в виде цифры");
+
+    }
 
 
+    else if (act == "X")
 
+    {
+        break;
+    }
+    else
+        Console.WriteLine("Wrong input");
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+Console.WriteLine("exit");
 
 
 
